a77dafa33bf062e09e1b6b919a3e49a0e613621e
mikenchi <mikenti337564@gmail.com>
2024-07-21 07:53:21 +0900

    [add]カメラ完成

--- Assets/Narita/CameraController.cs
+++ Assets/Narita/CameraController.cs
@@ -2,37 +2,56 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform _player;  // プレイヤーのTransform
-    [SerializeField] Vector3 _offset = new Vector3(0, 4.11f, -9.67f);  // プレイヤーとカメラの相対位置 = 中心に映したいときは使わない
     [SerializeField] float _smoothTime = 0.3f;  // カメラがプレイヤーを追跡する際のスムーズさの調整用パラメータ
-    [SerializeField] float _cameraradius = 20f;
+    [SerializeField] float _cameraradius = 20f;//カメラの回転する半径
+    [SerializeField] float _camerahigh;//カメラの高さ
+    [SerializeField] float _mousesensivity;//マウス感度
     private Vector3 velocity = Vector3.zero;  // カメラ移動時の速度ベクトル
     float th;
-    Vector3 cycle;
-    Vector3 mousePos;
+    float mousePos;
     float mousex;
     float mousedeltax;
+    float flame;
+    Vector3 _offset;
+    Vector3 cycle;
 
 
 
 
-    // プレイヤー移動後にカメラ移動をさせたいので、LateUpdateを使う
+
+   //堤 伎六作シネマシンを使わずにカメラ移動
     private void Update()
     {
+        flame += 1; 
+        Cursor.lockState = CursorLockMode.None;
+
         Cursor.visible = false;
-        mousePos = Input.mousePosition;
-        mousedeltax = mousePos.x - mousex;
-        mousex = mousePos.x;
-        th += mousedeltax /300;
-        mousePos = new Vector3(0, 0, 0);
+        mousePos= Input.GetAxis("Mouse X");
+      
+        if(_mousesensivity >= 0.1f)
+        {
+            th += mousePos / _mousesensivity;
+        }
+        else
+        {
+            th += mousePos;
+        }
         float x =_cameraradius * Mathf.Cos(th);
         float z = _cameraradius * Mathf .Sin(th);
 
 
-        _offset = new Vector3(x,10, z);
+        _offset = new Vector3(x,_camerahigh, z);
+       
+      
+       if(flame > 5)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            flame = 0;
+        }
         
-        
-        
+
     }
+    // プレイヤー移動後にカメラ移動をさせたいので、LateUpdateを使う
     void LateUpdate()
     {
         // プレイヤーの位置にカメラを追従させる
@@ -50,4 +69,3 @@ public class CameraController : MonoBehaviour
         }
     }
 }
-//調べたやつ
