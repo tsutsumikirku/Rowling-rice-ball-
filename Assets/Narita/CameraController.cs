89d712ef7120f54f8d3da202b0d2c290a225393d
mikenchi <mikenti337564@gmail.com>
2024-07-20 22:31:13 +0900

    [add]カメラ操作プログラム(プロトタイプ)

--- Assets/Narita/CameraController.cs
+++ Assets/Narita/CameraController.cs
@@ -5,7 +5,29 @@ public class CameraController : MonoBehaviour
     [SerializeField] Vector3 _offset = new Vector3(0, 4.11f, -9.67f);  // プレイヤーとカメラの相対位置 = 中心に映したいときは使わない
     [SerializeField] float _smoothTime = 0.3f;  // カメラがプレイヤーを追跡する際のスムーズさの調整用パラメータ
     private Vector3 velocity = Vector3.zero;  // カメラ移動時の速度ベクトル
+    float th;
+    Vector3 cycle;
+    Vector3 mousePos;
+    float mouse;
+    float mousedelta;
+
+
     // プレイヤー移動後にカメラ移動をさせたいので、LateUpdateを使う
+    private void Update()
+    {
+        
+        mousePos = Input.mousePosition;
+        mousedelta = mousePos.x - mouse;
+        mouse = mousePos.x;
+        th += mousedelta /1000;
+        
+        
+        float x =20 * Mathf.Cos(th);     
+        float y =20 * Mathf.Sin(th);
+        _offset = new Vector3(x,4, y);
+        
+        
+    }
     void LateUpdate()
     {
         // プレイヤーの位置にカメラを追従させる
